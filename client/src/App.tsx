d414d4a3c5ed623879b3ce2167079581f71abe1f
Ramsey Gavin <44215916+rgavin1@users.noreply.github.com>
2022-05-31 00:20:49 -0700

    Add rough login page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Stack, Button, Container, Drawer, Link, TextField } from '@mui/material';
 
 const App: React.FC = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleDrawer = () => {
+    setIsOpen(!isOpen)
+  }
   return (
-    <div className="app">
-      Self App
-    </div>
+    <Container className="app">
+      <Stack
+        direction="column"
+        justifyContent="center"
+        alignItems="stretch"
+      >
+        {isOpen ?
+          <h1>Logged In</h1> :
+          <h1>Logged Out</h1>
+        }
+        <Button fullWidth size="large" variant="contained" onClick={toggleDrawer}>Login</Button>
+        <p>Don't have an account? <Link href="#">Sign up</Link></p>
+      </Stack>
+      {
+        <React.Fragment key={"bottom"}>
+          <Drawer
+            anchor={"bottom"}
+            open={isOpen}
+            onClose={toggleDrawer}
+          >
+            <Container>
+              <Stack
+                direction="column"
+                justifyContent="flex-end"
+                alignItems="center"
+                spacing={1}
+              >
+                <h1>Welcome Back</h1>
+                <h5>Login to your account</h5>
+              </Stack>
+
+              <TextField fullWidth id="outlined-basic" label="Outlined" variant="outlined" />
+              <TextField fullWidth id="outlined-basic" label="Outlined" variant="outlined" />
+              <Button fullWidth size="large" variant="contained" onClick={toggleDrawer}>Login</Button>
+              <Link href="#">Forgot your password?</Link>
+              <p>Don't have an account? <Link href="#">Sign up</Link></p>
+            </Container>
+          </Drawer>
+        </React.Fragment>
+      }
+    </Container>
   );
 }
 
